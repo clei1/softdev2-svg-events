41426aa7fa18d91e62dd3a3cabbc43c5551741d4
clei1 <clei1@stuy.edu>
2018-03-06 22:50:04 -0500

    ugh

---
+++ index.js
@@ -0,0 +1,23 @@
+svg = document.getElementById("svg_id");
+
+var drawCircle = function(x, y){
+    var cir = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    cir.setAttribute("cx", x);
+    cir.setAttribute("cy", y);
+    cir.setAttribute("r", 10);
+    cir.addEventListener("click", changeColor);
+    svg.appendChild(cir);
+};
+
+var changeColor = function(){
+    event.stopPropagation();
+    this.setAttribute("fill", "green");
+    this.addEventListener("click", function(){
+	svg.removeChild(this);
+	drawCircle(Math.random() * 500, Math.random() * 500);
+    });
+};
+
+svg.addEventListener("click", function(){
+    drawCircle(event.offsetX, event.offsetY);
+});
